2e60eba3c2a01ee3dc658b011c09be6669705bea
Ayşenur Karapınar <aysenrkp0@gmail.com>
2021-03-08 22:50:45 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _203801055_aysenur_karapinar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[5];
+            for (int i = 0; i < 5; i++) ;
+            {
+                Console.Write("{0}.Sayıyı Giriniz", i+1);
+                sayilar[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(sayilar);
+            Console.WriteLine();
+            Console.Write("Sayıları Küçükten Büyüğe Sıralama=");
+            for (int i = 0; i < 5; i++) ;
+            {
+                Console.Write(sayilar[ i ] + " ");
+            }
+            Console.WriteLine();
+            Console.Write( " Sayıları Büyükten Küçüğe Sıralama= ");
+            Array.Reverse(sayilar);
+            for (int i = 0; i < 5; i++) ;
+            {
+                Console.WriteLine(sayilar[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
